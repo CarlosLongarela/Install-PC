41ce9d287b904867aa62cfc4001a199cd5292289
Carlos Longarela <carlos@longarela.eu>
2020-12-28 23:38:27 +0100

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,40 @@
+@ECHO OFF
+ECHO Now we'll start to install PC Software
+PAUSE
+
+ECHO Install browsers
+winget install Microsoft.Edge
+winget install Google.Chrome
+winget install Mozilla.Firefox
+winget install Mozilla.FirefoxDeveloperEdition
+winget install ResponsivelyApp.ResponsivelyApp
+winget install VivaldiTechnologies.Vivaldi
+
+ECHO Install Utilities
+winget install VB-Audio.VoiceMeeter_Banana
+winget install Twilio.Authy
+winget install 7zip.7zip
+winget install RescueTime.RescueTime
+winget install Microsoft.PowerToys
+winget install Greenshot.Greenshot
+winget install LibreOffice.LibreOffice 
+
+ECHO Install Dev Tools
+winget install Git.Git
+winget install Atlassian.Sourcetree
+winget install WPEngine.Local
+winget install Microsoft.VisualStudioCode
+winget install Microsoft.WindowsTerminal
+winget install HeidiSQL.HeidiSQL
+winget install Postman.Postman
+
+ECHO Install Communication & video Tools
+winget install Loom.Loom
+winget install Zoom.Zoom
+winget install Spotify.Spotify
+winget install StefanMalzner.Franz
+winget install Microsoft.Skype
+winget install Discord.Discord
+
+ECHO Install Other
+winget install Dropbox.Dropbox
